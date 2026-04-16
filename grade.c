49fe425e4bc9435cf809f24317b4ad8af3cded2c
João Francisco Scoloski Fassina <jf.fassina@gmail.com>
2026-04-16 11:48:50 -0300

    new: main.c, grade.c & funcoes.h

---
+++ grade.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main (char[]][] args){
+  int n = scanf("%d");
+  char grid[2][n];
+  scanf("%s",grid[0]);
+  scanf("%s",grid[1])
+
+  for(int i = 0; i < 2; i++){
+    for(int j = 0; j < n; j++){
+      if(grid[i][j] == '0'){
+        //É importante
+
+      }
+
+    }
+  }
+
+  
+}
+// USAR BFS
+// a cada iteracao trocar o 0 por 1 e dai rodar!!!!
+// ou seja fazer um caminhozinho
+// 0 0 0 0 0
+// 1 1 0 1 0
+// > > > > V 
+// x x V x V
