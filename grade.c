d95e7e5b9707f438be54fac9b6945638a270551a
João Francisco Scoloski Fassina <jf.fassina@gmail.com>
2026-04-16 14:48:37 -0300

    update: start on bfs logic

--- grade.c
+++ grade.c
@@ -1,28 +1,57 @@
 #include <stdio.h>
 
-int main (char[]][] args){
-  int n = scanf("%d");
-  char grid[2][n];
-  scanf("%s",grid[0]);
-  scanf("%s",grid[1])
+int n;
+char grid[2][10000001]; // 2 <= n <= 10^7 (o + 1 é para o '\0')
+int visitado[2][10000001];
 
-  for(int i = 0; i < 2; i++){
-    for(int j = 0; j < n; j++){
-      if(grid[i][j] == '0'){
-        //É importante
+void dfs(int linha, int coluna) {
+  // fora da grid
+  if (linha < 0 || linha >= 2)
+    return;
+  if (coluna < 0 || coluna >= n)
+    return;
 
-        //verificar se existem movimentos possiveis
-      }
+  // bloqueado ou já visitado
+  if (grid[linha][coluna] == '1' || visitado[linha][coluna])
+    return;
 
-    }
-  }
+  visitado[linha][coluna] = 1;
 
-  
+  // movimentos possíveis
+  dfs(linha, coluna + 1); // direita
+  dfs(linha, coluna - 1); // esquerda
+  dfs(1 - linha, coluna); // troca de linha
 }
-// USAR BFS
-// a cada iteracao trocar o 0 por 1 e dai rodar!!!!
-// ou seja fazer um caminhozinho
-// 0 0 0 0 0
-// 1 1 0 1 0
-// > > > > V 
-// x x V x V
+
+int main() {
+  scanf("%d", &n);
+
+  scanf("%s", grid[0]);
+  scanf("%s", grid[1]);
+
+  dfs(0, 0);
+
+  if (visitado[1][n - 1])
+    printf("YES\n");
+  else
+    printf("NO\n");
+
+  return 0;
+}
+/* USAR BFS
+ a cada iteracao trocar o 0 por 1 e dai rodar!!!!
+ ou seja fazer um caminhozinho
+ 0 0 0 0 0
+ 1 1 0 1 0
+ > > > > V
+ x x V x V
+
+ start:
+ * visitar(nó)
+ *   marcar como visitado
+ *
+ *  para cada vizinho:
+ *    se não foi visitado:
+ *        visitar(vizinho)
+ end;
+ */
