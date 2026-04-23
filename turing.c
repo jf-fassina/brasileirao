bbd4559cd92d9fd5afe10bd7bbf221e69a24cf96
João Fassina <158244312+jf-fassina@users.noreply.github.com>
2026-04-23 14:01:15 -0300

    Update turing.c

--- turing.c
+++ turing.c
@@ -3,26 +3,15 @@
 #include <string.h>
 
 int main(void) {
-
-  int l;
   char b[100];
   char *e;
-  l = strtol(fgets(b, 100, stdin), &e, 10);
-
+  int l = strtol(fgets(b, 100, stdin), &e, 10);
   for (int i = 0; i < l; i++) {
     char p[10000];
     fgets(p, 10000, stdin);
     p[strcspn(p, "\n")] = '\0';
     int n = strlen(p);
-    int k = 0;
-    char o = p[n - 1];
-    while (1) {
-      char y = ((o - 'A') - k + 26) % 26 + 'A';
-      if (y == 'Y') {
-        break;
-      }
-      k++;
-    }
+    int k = ((p[n - 1] - 'A') - 24 + 26) % 26;
     for (int j = 0; j < n; j++) {
       if (p[j] >= 'A' && p[j] <= 'Z') {
         p[j] = ((p[j] - 'A') - k + 26) % 26 + 'A';
@@ -30,6 +19,5 @@ int main(void) {
     }
     printf("%s\n", p);
   }
-
   return 0;
 }
